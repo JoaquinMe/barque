6b16d979bb95fbd455c60ce7c7ed6e161852c469
Joaquin Messano <joaquin.messano@gmail.com>
2025-01-17 23:12:39 -0300

    remove test scripts

--- 14_tests/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-import subprocess
-import re
-
-result=subprocess.run(["shasum",f"14_tests/test.py"],capture_output=True)
-checksum=re.match(r"^(\w*) ",result.stdout.decode("utf-8"))
-print([checksum.group(1)])
