b4a3c99bace71a1c6eff7946b61027e40afecb58
Joaquin Messano <joaquin.messano@gmail.com>
2025-02-17 15:10:45 -0300

    Update 13_annotate.py

--- 01_scripts/13_annotate.py
+++ 01_scripts/13_annotate.py
@@ -1,6 +1,10 @@
 import gzip
 import sys
 
+import pandas as pd
+from Bio import Entrez
+from Bio.Blast import NCBIWWW, NCBIXML
+
 
 # Defining classes
 class Fasta(object):
@@ -21,6 +25,46 @@ def myopen(infile, mode="rt"):
         return gzip.open(infile, mode=mode)
     else:
         return open(infile, mode=mode)
+
+
+def calculate_percentage_identity(hsps):
+    """
+    Calculate percentage identity between the query and subject sequences based on HSPs.
+    """
+    total_identities = 0
+    total_aligned_length = 0
+
+    for hsp in hsps:
+        total_identities += hsp.identities  # Number of identical residues
+        total_aligned_length += hsp.align_length  # Total length of the alignment
+
+    # Calculate percentage identity
+    if total_aligned_length > 0:
+        percentage_identity = (total_identities / total_aligned_length) * 100
+    else:
+        percentage_identity = 0
+
+    return percentage_identity
+
+
+def blast_search(sequence):
+    cols = ["accession", "pid"]
+    df = pd.DataFrame(columns=cols)
+    # Perform a BLAST search using the NCBIWWW.qblast function
+    result_handle = NCBIWWW.qblast("blastn", "nt", sequence)
+
+    # Parse the BLAST result
+    blast_records = NCBIXML.parse(result_handle)
+
+    # Iterate through the BLAST records
+    for blast_record in blast_records:
+        for i, alignment in enumerate(blast_record.alignments):
+            accession = alignment.accession
+            percentage_identity = calculate_percentage_identity(alignment.hsps)
+            newrow = {"accession": accession, "pid": percentage_identity}
+            df.loc[i] = newrow
+
+    return df
 
 
 def fasta_iterator(input_file):
@@ -54,5 +98,11 @@ except:
 
 fasta_sequences = fasta_iterator(fasta_file)
 
+count = 0
 for seq in fasta_sequences:
-    print(seq.name)
+    if count == 0:
+        count += 1
+        data = blast_search(seq.sequence)
+        print(data)
+    else:
+        break
