0183367b43a948aa1916ad170a4ca1444b9a66a9
Joaquin Messano <joaquin.messano@gmail.com>
2025-02-18 21:02:18 -0300

    Update test.py

--- 01_scripts/test.py
+++ 01_scripts/test.py
@@ -1,4 +1,25 @@
 import gzip
+import os
 import sys
 
 import pandas as pd
+
+pid_cutoff = float(sys.argv[1]) * 100  # porcentaje de similaridad para cortar
+df = pd.read_csv("01_scripts/test.csv")
+df = df.loc[df["pid"] >= pid_cutoff]
+taxtable = df["tax"].str.split("; ", expand=True)
+taxtable_const = taxtable.copy()
+keep = True
+taxlist = []
+i = 0
+print(taxtable.head())
+while keep and taxtable.shape[1]>i:
+    col = taxtable.iloc[:, i]
+    if col.unique().size == 1:
+        keep = True
+        taxlist.append(col.unique()[0])
+        i+=1
+    else:
+        keep = False
+
+print(taxlist)
